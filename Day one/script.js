0e412235df7bab1405da2ec69e092e1e80dae6cd
Bibek Joshi <bebikjoshi118@gmail.com>
2025-02-23 19:07:27 +0545

    day one

---
+++ Day one/script.js
@@ -0,0 +1,63 @@
+let x = "biebk Joshi"
+console.log(typeof(x));
+x=25;
+console.log(typeof(x));
+
+
+let b= 12345;
+console.log(typeof(b));
+let  y= String(b);  // Type conversion //
+console.log(typeof(y));
+
+
+
+let initialTime = new Date();
+console.log(typeof(initialTime));
+
+
+let newString = String(initialTime);
+console.log(initialTime);
+
+
+
+var  z= 12345;
+var  z = "Ram ";
+
+
+const g = 9.81;
+// g= 6.21;
+console.log(g)
+
+var c;
+var c="";
+
+
+// let v=25;
+// let  w=20;
+// console.log("The sum of " )
+
+
+//  Templete literal // 
+
+let v=25;
+let w= 20;
+console.log(`The sum 
+of ${v} and ${w} is ${v+w}`);
+
+
+// let e=25;
+// let f= 20;
+// console.log("the sum of ", a, )
+
+
+// control structure//
+  // If else statements //
+let num =10;
+
+if (num > 0 ){
+    console.log("positive number ");
+
+}
+else{
+    console.log("negative number ");
+}
