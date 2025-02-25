ad7978654552259f3bdbd79989e0111f2bf70e62
Bibek Joshi <bebikjoshi118@gmail.com>
2025-02-25 18:36:54 +0545

    day 3 dom

---
+++ day three/script.js
@@ -0,0 +1,17 @@
+// alert("welcome to Javascript Day Three ");
+
+// document.getElementById('firstheading').style.backgroundColor="pink";
+
+// // document.getElementsByClassName('paragraph').style.backgroundColor="aqua";
+
+// document.getElementById('firstheading').style.color="white";
+
+document.getElementById('checknumber').addEventListener('click',()=>{
+let x= document.getElementById('number').value;
+if(x%2==0){
+    alert ("even");
+}
+else{
+    ("odd");
+
+}})
